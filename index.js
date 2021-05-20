8ca61be086f9ceee30930cd51988840008867ccd
Steve <catholickiwi2016@gmail.com>
2021-05-20 17:21:00 +1200

    Add dbConfig to set up for deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const server = require("./api/server");
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server is Listening on Port:  ${PORT}`);
+server.listen(port, () => {
+  console.log(`Server is Listening on Port:  ${port}`);
 });
